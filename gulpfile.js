509522a6e5265aaca03289d885907680afadd0a2
angeal185 <beneaves01@hotmail.com>
2017-06-24 01:31:10 +0800

    update

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require("gulp"),
+requireDir  = require("require-dir"),
+gutil 		= require("gulp-util"),
+chalk 		= require("chalk"); 
+requireDir("./gulp-tasks", {recurse: true}),
+nunjucksRender = require("gulp-nunjucks-render"),
+data = require('gulp-data');
+
+gulp.task('nunjucks', function() {
+  return gulp.src('views/pages/**/*.njk')
+    .pipe(data(function() {
+      return require('./app/json/data.json')
+    }))
+	.pipe(nunjucksRender({
+      path: ['views/templates']
+    }))
+	.pipe(gulp.dest('./includes'))
+});
+
+
+
+
+gulp.task("default", ["start"], function() {
+	console.log(chalk.blue('Server listening with tasks:'),chalk.green(' Start'))
+});
