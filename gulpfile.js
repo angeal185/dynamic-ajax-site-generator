801f209e6396e6ab1b6235914f037b304d913430
angeal185 <beneaves01@hotmail.com>
2017-06-24 01:24:38 +0800

    update

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require("gulp"),
-requireDir  = require("require-dir"),
-gutil 		= require("gulp-util"),
-chalk 		= require("chalk"); 
-requireDir("./gulp-tasks", {recurse: true});
-
-gulp.task("default", ["start"], function() {
-	console.log(chalk.blue('Server listening with tasks:'),chalk.green(' Start'))
-});
